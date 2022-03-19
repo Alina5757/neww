ff8e5aa464db705af6c1c31cfc6dd98dc0f8e519
Алина Жукова <445390@mail.ru>
2022-03-19 11:56:47 +0400

    Добавьте файлы проекта.

---
+++ CannedFactoryContracts/BusinessLogicsContracts/ICannedLogic.cs
@@ -0,0 +1,17 @@
+﻿using CannedFactoryContracts.BindingModels;
+using CannedFactoryContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CannedFactoryContracts.BusinessLogicsContracts
+{
+    public interface ICannedLogic
+    {
+        List<CannedViewModel> Read(CannedBindingModel model);
+
+        void CreateOrUpdate(CannedBindingModel model);
+
+        void Delete(CannedBindingModel model);
+    }
+}
---
+++ CannedFactoryView/FormCanneds.cs
@@ -0,0 +1,89 @@
+﻿using CannedFactoryContracts.BindingModels;
+using CannedFactoryContracts.BusinessLogicsContracts;
+using System;
+using Unity;
+using System.Windows.Forms;
+
+namespace CannedFactoryView
+{
+    public partial class FormCanneds : Form
+    {
+        private readonly ICannedLogic _logic;
+        public FormCanneds(ICannedLogic logic)
+        {
+            InitializeComponent();
+            _logic = logic;
+            LoadData();
+        }
+
+        private void FormCanneds_Load(object sender, EventArgs e) {
+            LoadData();
+        }
+
+        private void LoadData() {
+            try
+            {
+                var list = _logic.Read(null);
+                if (list != null)
+                {
+                    dataGridView1.DataSource = list;
+                    dataGridView1.Columns[0].Visible = false;
+                    dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[2].Width = 75;
+                    dataGridView1.Columns[3].Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Program.Container.Resolve<FormCanned>();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                LoadData();
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                var form = Program.Container.Resolve<FormCanned>();
+                form.Id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        _logic.Delete(new CannedBindingModel { Id = id });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRefact_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
